5ad7e80cbc61c685ab4a2119ab988c18fb1af961
Leejeongin <jeonginlee72@gmail.com>
2022-12-05 19:36:47 +0900

    자잘한 수정

--- Board.cpp
+++ Board.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "CWall.h"
 #include "CSphere.h"
-
+#include <random>
 
 using namespace std;
 
@@ -16,31 +16,61 @@ private:
 
 public:
 
-    Board() {
-        //        x_bdCtr = gamemanager.getcenter_x();
-        //        y_bdCtr = gamemanager.getcenter_y();
-        //        z_bdCtr = gamemanager.getcenter_z();
+    //Board() {
+    //            x_bdCtr = gamemanager.getcenter_x();
+    //            y_bdCtr = gamemanager.getcenter_y();
+    //            z_bdCtr = gamemanager.getcenter_z();
 
 
 
-    }
-    Board(float X, float Y, float Z) {
+    //}
+    Board(float X, float Y, float Z, IDirect3DDevice9* pDevice) {
 
         this->x_bdCtr = X;
         this->y_bdCtr = Y;
         this->z_bdCtr = Z;
 
-        for (int i = 0; i < sizeof(brd) / sizeof(*brd); i++) {
-            if (i % 2 == 0) {
-                // 중심 기준으로 공의 x z 초기좌표 설정
-                // col[i]line[j]_x = x_bdCtr-(line->size()/2)+0.5+j ball.center_x =
-                // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
-            }
-            else if (i % 2 == 1) {
-                // col[i]line[j]_x = x_bdCtr-(line->size()/2)+1+j
-                // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> dis(1, 4);
+
+        // 중심 기준으로 공의 x z 초기좌표 설정
+
+        for (int i = 0; i < sizeof(brd) / sizeof(brd[0]); i++) //row
+        {
+            for (int j = 0; j < sizeof(brd) / sizeof(CSphere); j++) //col
+            {
+
+				brd[i][j].create(pDevice);
+
+                int color = dis(gen);
+				if (color == 1)
+					brd[i][j].setColor(d3d::YELLOW);
+				else if (color == 2)
+					brd[i][j].setColor(d3d::RED);
+                else if(color==3)
+                    brd[i][j].setColor(d3d::BLUE);
+                else
+                    brd[i][j].setColor(d3d::GREEN);
+
+                //set center만 넣어주세요
+
+                if (i > 3)
+                    brd[i][j].set_exist(false);
             }
         }
+
+        //for (int i = 0; i < sizeof(brd) / sizeof(*brd); i++) {
+        //    if (i % 2 == 0) {
+        //        // 중심 기준으로 공의 x z 초기좌표 설정
+        //        // col[i]line[j]_x = x_bdCtr-(line->size()/2)+0.5+j ball.center_x =
+        //        // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
+        //    }
+        //    else if (i % 2 == 1) {
+        //        // col[i]line[j]_x = x_bdCtr-(line->size()/2)+1+j
+        //        // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
+        //    }
+        //}
     }
 
     void destroy(int m, int n, int col) {
--- Csphere.h
+++ Csphere.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "d3dUtility.h"
 
-#define M_RADIUS 0.21   // ball radius
+#define M_RADIUS 0.5   // ball radius
 #define PI 3.14159265
 #define M_HEIGHT 0.01
 #define DECREASE_RATE 0.9982
