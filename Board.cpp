3f3b3e6d2dd4474d1cf95ee5d90c805eeda942da
hungeredJims <81881724+codingjiemangsaeng@users.noreply.github.com>
2022-12-07 01:02:32 +0900

    Update Board.cpp
    떨어지는 볼 갯수 return 추가
    천장에 붙는 경우 추가했는데, 이 경우 매니저에서 index m,n 을 -1 처리해서 넣어주면 됩니다.
    볼 위치 선정도 수정했는데, 이건 보면서 계속 수정해 나갈 생각입니다.

--- Board.cpp
+++ Board.cpp
@@ -1,153 +1,199 @@
-//
-//  Board.cpp
-//  oop_prj4
-//
-//  Created by 정지현 on 12/3/22.
-//
-
 #include <stdio.h>
 #include <vector>
 #include "CWall.h"
 #include "CSphere.h"
-
+#include <random>
 
 using namespace std;
 
-class Board{
-    
-    private :
-    CSphere brd [10][8];
+class Board {
+
+private:
+    CSphere brd[10][8];
+    CSphere extBrd [15];
     float x_bdCtr, y_bdCtr, z_bdCtr;
     int rBoundary;
     int empFl = -3;
-    public :
 
-    Board (){
-//        x_bdCtr = gamemanager.getcenter_x();
-//        y_bdCtr = gamemanager.getcenter_y();
-//        z_bdCtr = gamemanager.getcenter_z();
-        
-        
+public:
 
-    }
-    Board (float X, float Y, float Z, float depth){
-        
+    //Board() {
+    //            x_bdCtr = gamemanager.getcenter_x();
+    //            y_bdCtr = gamemanager.getcenter_y();
+    //            z_bdCtr = gamemanager.getcenter_z();
+    //}
+
+
+
+    void create(float X, float Y, float Z, IDirect3DDevice9* pDevice, float depth) {
+
         this->x_bdCtr = X;
         this->y_bdCtr = Y;
         this->z_bdCtr = Z;
-        
+
         rBoundary = 3;
-        
-        for (int i = 0; i<rBoundary; i++) {
-            for (int j = 0; j<sizeof(brd[0])/sizeof(*brd[0]); j++) {
-                // 이 부분만 합쳐주시면 됩니다.
-                if (i%2 == 0) {
-                    brd[i][j].setCenter(x_bdCtr-(sizeof(brd[0])/sizeof(*brd[0])/2-0.5-j)*brd[i][j].getRadius()/0.5,0,z_bdCtr + depth/2-i*brd[i][j].getRadius()/0.5);
-                    // col[i]line[j]_x = x_bdCtr-(line->size()/2)+0.5+j ball.center_x =
-                    // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
+
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> dis(1, 4);
+
+
+        for (int i = 0; i < sizeof(brd) / sizeof(brd[0]); i++) //row
+        {
+            for (int j = 0; j < sizeof(brd[0]) / sizeof(CSphere); j++) //col
+            {
+                brd[i][j].create(pDevice);
+
+                int color = dis(gen);
+                if (color == 1)
+                    brd[i][j].setColor(d3d::YELLOW);
+                else if (color == 2)
+                    brd[i][j].setColor(d3d::RED);
+                else if (color == 3)
+                    brd[i][j].setColor(d3d::BLUE);
+                else
+                    brd[i][j].setColor(d3d::GREEN);
+
+                if (i % 2 == 0) {
+                    brd[i][j].setCenter(x_bdCtr - (sizeof(brd[0]) / sizeof(*brd[0]) / 2 - 0.5 - j) * brd[i][j].getRadius() / 0.5, 0, z_bdCtr + depth / 2 - i * brd[i][j].getRadius() / 0.5);
                 }
-                else if (i%2 ==1){
-                    brd[i][j].setCenter(x_bdCtr-(sizeof(brd[0])/sizeof(*brd[0])/2-j)*brd[i][j].getRadius(),0,z_bdCtr + depth/2-i*brd[i][j].getRadius());
-                    // col[i]line[j]_x = x_bdCtr-(line->size()/2)+1+j
-                    // col[i]line[j]_z = [z_bdCtr+(boardlength/2)]-i
+                else if (i % 2 == 1) {
+                    brd[i][j].setCenter(x_bdCtr - (sizeof(brd[0]) / sizeof(*brd[0]) / 2 - j) * brd[i][j].getRadius()/0.5, 0, z_bdCtr + depth / 2 - i * brd[i][j].getRadius()/0.5);
+                }
+
+                if (i > 3)
+                {
+                    brd[i][j].setExist(false);
+                    brd[i][j].setColor(d3d::MAGENTA);
                 }
             }
         }
-        
-        
     }
-    
-    ~Board(){}
-    
-    void destroy(int m, int n, int col){
-        chNeighball(m, n, col, 1);
-        for (int i = 0; i<sizeof(brd[0])/sizeof(*brd[0]); i++) {
-            if(brd[rBoundary][i].getExist())
+
+    int destroy(int m, int n, int col) {
+        this->chNeighball(m, n, col, 1);
+
+        //�ٿ���� �� ����ϴ� �ݺ���
+        int hit = 0;
+        
+        for (int i = 0; i < sizeof(brd[0]) / sizeof(*brd[0]); i++) {
+            if (brd[rBoundary][i].getExist())
                 break;
-            else if(i==sizeof(brd[0])/sizeof(*brd[0])&&(!brd[rBoundary][i].getExist())){
+            else if (i == sizeof(brd[0]) / sizeof(*brd[0]) && (!brd[rBoundary][i].getExist())) {
                 i = -1;
-                rBoundary --;
+                rBoundary--;
+
             }
         }
-        // 터트리기
-        
+        // �Ͷ߸��� �ݺ���
+        for (int i = 0; i < rBoundary; i++) {
+            for (int j = 0; i < sizeof(brd[0]) / sizeof(*brd[0]); j++) {
+                if (brd[i][j].getChflag() == 1) {
+                    //�Ͷ߸��� ����
+                    brd[i][j].setExist(false);
+                    brd[i][j].setColor(d3d::MAGENTA);
+                    hit++;
+
+                }
+            }
+        }
+        return hit;
     }
-    
-    void chNeighball(int m, int n, int col, int cs){
-        //ball 에 chflag 추가, getter, setter 도 마찬가지
-        if(brd[m][n].getChflag()==0){
-            if (brd[m][n].getColor()==col) {
+
+    void chNeighball(int m, int n, int col, int cs) {
+        //ball ??chflag 추�?, getter, setter ??마찬가지
+        if (brd[m][n].getChflag() == 0) {
+            if (brd[m][n].getColor() == col) {
                 brd[m][n].setChflag(cs);
-                
-                if (n<sizeof(brd[0])/sizeof(*brd[0])-1) {
-                    chNeighball(m, n+1, col, cs);
+
+                if (n < sizeof(brd[0]) / sizeof(*brd[0]) - 1) {
+                    chNeighball(m, n + 1, col, cs);
                 }
-                
-                if(m%2==0){
-                    if(m<=rBoundary){
-                        chNeighball(m+1, n, col, cs);
-                        if (n>0)
-                            chNeighball(m+1,n-1, col, cs);
-                        
+
+                if (m % 2 == 0) {
+                    if (m <= rBoundary) {
+                        chNeighball(m + 1, n, col, cs);
+                        if (n > 0)
+                            chNeighball(m + 1, n - 1, col, cs);
+
                     }
-                    if (n>0) {
-                        chNeighball(m, n-1, col, cs);
-                        if(m>0)
-                            chNeighball(m-1, n-1, col, cs);
+                    if (n > 0) {
+                        chNeighball(m, n - 1, col, cs);
+                        if (m > 0)
+                            chNeighball(m - 1, n - 1, col, cs);
                     }
-                    if(m>0)
-                        chNeighball(m-1, n, col, cs);
+                    if (m > 0)
+                        chNeighball(m - 1, n, col, cs);
                 }
-                
-                else if (m%2 ==1){
-                    if (m<=rBoundary) {
-                        if(n<sizeof(brd[0])/sizeof(*brd[0])-1)
-                            chNeighball(m+1, n+1, col, cs);
-                        
-                        chNeighball(m+1, n, col, cs);
+
+                else if (m % 2 == 1) {
+                    if (m <= rBoundary) {
+                        if (n < sizeof(brd[0]) / sizeof(*brd[0]) - 1)
+                            chNeighball(m + 1, n + 1, col, cs);
+
+                        chNeighball(m + 1, n, col, cs);
                     }
-                    if (n>0)
-                        chNeighball(m, n-1, col, cs);
-                    
-                    if (m>0) {
-                        chNeighball(m-1, n, col, cs);
-                        if(n<sizeof(brd[0])/sizeof(*brd[0])-1)
-                            chNeighball(m-1, n+1, col, cs);
+                    if (n > 0)
+                        chNeighball(m, n - 1, col, cs);
+
+                    if (m > 0) {
+                        chNeighball(m - 1, n, col, cs);
+                        if (n < sizeof(brd[0]) / sizeof(*brd[0]) - 1)
+                            chNeighball(m - 1, n + 1, col, cs);
                     }
-                    
                 }
-                
-                
-                
+
             }
         }
     }
-    
-    
-    
-    bool bAttach(int m, int n, CSphere& ball){
-        
+
+
+    void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld) {
+        for (int i = 0; i < sizeof(brd) / sizeof(brd[0]); i++) //row
+        {
+            for (int j = 0; j < sizeof(brd[0]) / sizeof(CSphere); j++) //col
+            {
+                if (brd[i][j].getExist())
+                    brd[i][j].draw(pDevice, mWorld);
+            }
+        }
+    }
+
+    bool bAttach(int m, int n, CSphere& ball) {
+
         float bCent_x = ball.getCenter().x;
-        
-        if (m<sizeof(brd)/sizeof(*brd)-1) {
-            if (bCent_x >= brd[m][n]) {
-                if (m%2==0) {
-                    brd[m+1][n].setColor(ball.getColor());
-                    brd[m+1][n].setExist(ball.getExist());
+
+        if (m < sizeof(brd) / sizeof(*brd) - 1) {
+            if (m==-1&&n==-1) {
+                int i;
+                for (i=0; i<sizeof(extBrd)/sizeof(*extBrd); i++) {
+                    if (!extBrd[i].getExist()) {
+                        break;
+                    }
                 }
-                else if(m%2==1){
-                    brd[m+1][n+1].setColor(ball.getColor());
-                    brd[m+1][n+1].setExist(ball.getExist());
+                extBrd[i].setColor(ball.getColor());
+                extBrd[i].setCenter(ball.getCenter().x,ball.getCenter().y,ball.getCenter().z);
+                extBrd[i].setExist(true);
+                
+            }
+            if (bCent_x >= brd[m][n].getCenter().x) {
+                if (m % 2 == 0) {
+                    brd[m + 1][n].setColor(ball.getColor());
+                    brd[m + 1][n].setExist(true);
+                }
+                else if (m % 2 == 1) {
+                    brd[m + 1][n + 1].setColor(ball.getColor());
+                    brd[m + 1][n + 1].setExist(true);
                 }
             }
-            else if (bCent_x < brd[m][n]){
-                if (m%2==0) {
-                    brd[m+1][n-1].setColor(ball.getColor());
-                    brd[m+1][n-1].setExist(ball.getExist());
+            else if (bCent_x < brd[m][n].getCenter().x) {
+                if (m % 2 == 0) {
+                    brd[m + 1][n - 1].setColor(ball.getColor());
+                    brd[m + 1][n - 1].setExist(true);
                 }
-                else if(m%2==1){
-                    brd[m+1][n].setColor(ball.getColor());
-                    brd[m+1][n].setExist(ball.getExist());
+                else if (m % 2 == 1) {
+                    brd[m + 1][n].setColor(ball.getColor());
+                    brd[m + 1][n].setExist(true);
                 }
             }
             rBoundary++;
@@ -155,87 +201,125 @@ class Board{
         }
         else return false;
     }
-    
-    void chEmpty(int m,int n, int* hMax, int *wMin, int* wMax){
-       
-        if(brd[m][n].getColor()==0){
+
+    void chEmpty(int m, int n, int* hMax, int* wMin, int* wMax) {
+
+        if (brd[m][n].getColor() == 0) {
             brd[m][n].setChflag(empFl);
-            if (m>*hMax)
+            if (m > *hMax)
                 *hMax = m;
-            if (n<*wMin)
+            if (n < *wMin)
                 *wMin = n;
-            else if (n>*wMax)
+            else if (n > *wMax)
                 *wMax = n;
-            
-            if(n>0)
-            chEmpty(m, n-1, hMax, wMin, wMax);
-            
-            if (m%2 == 0) {
-                
-                if(m>0){
-                    if (n>0) {
-                        chEmpty(m-1, n-1, hMax , wMin , wMax);
+
+            if (n > 0)
+                chEmpty(m, n - 1, hMax, wMin, wMax);
+
+            if (m % 2 == 0) {
+
+                if (m > 0) {
+                    if (n > 0) {
+                        chEmpty(m - 1, n - 1, hMax, wMin, wMax);
                     }
-                    chEmpty(m-1, n, hMax, wMin, wMax);
+                    chEmpty(m - 1, n, hMax, wMin, wMax);
                 }
-                
-                
-                chEmpty(m, n+1, hMax, wMin, wMax);
-                chEmpty(m+1, n, hMax, wMin, wMax);
-                chEmpty(m+1, n-1, hMax, wMin, wMax);
-                
+
+
+                chEmpty(m, n + 1, hMax, wMin, wMax);
+                chEmpty(m + 1, n, hMax, wMin, wMax);
+                chEmpty(m + 1, n - 1, hMax, wMin, wMax);
+
             }
-            else if(m%2 == 1){
-                
-                chEmpty(m-1, n, hMax , wMin , wMax);
-                chEmpty(m-1, n+1, hMax, wMin, wMax);
-                chEmpty(m, n+1, hMax, wMin, wMax);
-                chEmpty(m+1, n+1, hMax, wMin, wMax);
-                chEmpty(m+1, n, hMax, wMin, wMax);
+            else if (m % 2 == 1) {
+
+                chEmpty(m - 1, n, hMax, wMin, wMax);
+                chEmpty(m - 1, n + 1, hMax, wMin, wMax);
+                chEmpty(m, n + 1, hMax, wMin, wMax);
+                chEmpty(m + 1, n + 1, hMax, wMin, wMax);
+                chEmpty(m + 1, n, hMax, wMin, wMax);
             }
         }
-        
-                
-        
-                
-            
     }
-    
-    int bDetach(){
-        
+
+    int bDetach() {
+
         int i;
         int* min;
         int* max;
         int* hei;
-        
-        for (i = 0; i<sizeof(brd[0])/sizeof(*brd[0]); i++) {
-            if (brd[rBoundary][i].getColor == 0)
+        int hit = 0;
+        for (i = 0; i < sizeof(brd[0]) / sizeof(*brd[0]); i++) {
+            if (brd[rBoundary][i].getColor() == 0)
                 break;
         }
-        
+
         *hei = rBoundary;
         *min = i;
         *max = i;
-        
+
         chEmpty(rBoundary, i, hei, min, max);
-        
-        if(*hei<rBoundary && *min<*max){
-            for (int i = *hei; i<= rBoundary; i++) {
-                for (int j = *min; j<= *max; j++) {
-                    //터뜨리기
+
+        if (*hei < rBoundary && *min < *max) {
+            for (int i = *hei; i <= rBoundary; i++) {
+                for (int j = *min; j <= *max; j++) {
+                    //�Ͷ߸���
+                    if (brd[i][j].getChflag() == -2)
+                    {
+                        brd[i][j].setExist(false);
+                        brd[i][j].setColor(d3d::MAGENTA);
+                        hit++;
+                    }
                 }
             }
         }
-        
+        return hit;
     }
-    
-    void resume(){
-        
+
+    void resume(int col, int row, float x, float y, float z, int color, bool exist)
+    {
+        brd[row][col].setCenter(x, y, z);
+
+        if (color == 1)
+        {
+            brd[row][col].setColor(d3d::YELLOW);
+        }
+        else if (color == 2)
+        {
+            brd[row][col].setColor(d3d::RED);
+
+        }
+        else if (color == 3)
+        {
+            brd[row][col].setColor(d3d::BLUE);
+        }
+        else if (color == 4)
+        {
+            brd[row][col].setColor(d3d::GREEN);
+        }
+        else
+        {
+            brd[row][col].setColor(d3d::MAGENTA);
+        }
+
+        brd[row][col].setExist(exist);
     }
-    
-    void draw(){
-        
+
+    CSphere getBall(int row, int col)
+    {
+        return this->brd[row][col];
     }
+
+    int getRow()
+    {
+        return sizeof(brd) / sizeof(brd[0]);
+    }
+
+    int getCol()
+    {
+        return sizeof(brd[0]) / sizeof(CSphere);
+    }
+
 };
 
 
